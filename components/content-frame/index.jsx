888d9b411d870638a43b174cf11ad79869f3d3b5
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-11 15:30:53 +1300

    added mouse based offset to content fram borders

--- components/content-frame/index.jsx
+++ components/content-frame/index.jsx
@@ -2,13 +2,55 @@ import React, { Component } from 'react';
 import './style.css'
 
 class ContentFrame extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      mouseX:0,
+      mouseY:0
+    }
+    this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
+  handleMouseMove(e){
+    this.setState(
+      {
+        mouseX:e.clientX,
+        mouseY:e.clientY
+      }
+    )
+  }
+  componentDidMount() {
+    window.addEventListener("mousemove",this.handleMouseMove)
+  }
+  calculateOffset(mouseX, mouseY, maxOffsetAmount = 4){
+    const halfWindowWidth = window.outerWidth / 2
+    const halfWindowHeight = window.outerHeight / 2
+    const x =  ((mouseX - halfWindowWidth) / halfWindowWidth) * maxOffsetAmount
+    const y =  ((mouseY - halfWindowHeight) / halfWindowHeight) * maxOffsetAmount
+    return({x,y})
+  }
   render() {
+    const offset=this.calculateOffset(this.state.mouseX,this.state.mouseY)
     return (
-      <div ref={ (element) => { this._element = element}} className={"frame"}>
-        <span className={'frame-edge-left'}></span>
+      <div className={"frame"}>
+        <span 
+          className={'frame-edge-left'}
+          style={
+            {
+                transform:`translate(${offset.x}px, ${offset.y}px)`,
+            }
+          }
+        >
+        </span>
         {this.props.children}
-        <span className={'frame-edge-right'}></span>        
+        <span 
+          className={'frame-edge-right'}
+          style={
+            {
+                transform:`translate(${offset.x}px, ${offset.y}px)`,
+            }
+          }
+        >
+        </span>        
       </div>
     );
   }
