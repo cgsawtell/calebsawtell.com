10ca7a57bff516b0bf3ca5288b0795df90d3dbd4
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-12 11:27:10 +1300

    added markup and skeleton handlers for contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import ContentFrame from '../components/content-frame';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      email:'',
+      message:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state);
+  }
+  handleChange(e){
+    let updatedField = {}
+    updatedField[e.target.name] = e.target.value
+    this.setState(updatedField)
+  }
   render() {
     return (
       <div>
-        
+        <ContentFrame title={"Contact"}>
+        <form onSubmit={e => { this.handleSubmit(e) }}>
+          <label for={"name"}>Name</label>
+          <input id="name" name={'name'} type="text" onChange={e => this.handleChange(e)}/>
+          <label for={"email"}>Email</label>
+          <input id="email" name={'email'} type="email" onChange={e => this.handleChange(e)}/>
+          <label for={"message"}>Message</label>
+          <textarea name="message" id="message" cols="30" rows="10" onChange={e => this.handleChange(e)}></textarea>
+          <input type="submit" value={"submit"}/>
+        </form>
+        </ContentFrame>
       </div>
     );
   }
