4696921994b2ea5bc938584af07fbfdaf221cfcf
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-12 11:26:41 +1300

    tidied up three-dee component and added geometry parsing

--- components/three-dee/index.jsx
+++ components/three-dee/index.jsx
@@ -1,41 +1,66 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import monkeyJSON from '../../assets/meshes/monkey.json';
+
 class ThreeDee extends Component {
   constructor(props) {
     super(props);
     this.animate = this.animate.bind(this)
-  }
-  
+  }  
   componentDidMount() {
     this.setup()
     this.animate()
   }
   setup(){
-    this._scene = new THREE.Scene();
-    this._camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    this._camera.position.z = 1000;
-
-    this._geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    this._material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-    this._mesh = new THREE.Mesh( this._geometry, this._material );
-    this._scene.add( this._mesh );
-
-    this._renderer = new THREE.WebGLRenderer({canvas:this._canvas,alpha:true});
-    this._renderer.setSize( window.innerWidth, window.innerHeight );
+    this.scene = new THREE.Scene();
+    this.setupRenderer();
+    this.setupCamera()
+    // const renderPasses = this.createPasses();
+    // this.setupComposer(renderPasses);
+    this.mesh = this.monkeyMesh()
+    this.scene.add( this.mesh );
+  }
+  createPasses(){
+    const renderPass = new THREE.RenderPass( this.scene, this.camera );
+    const effectBloom = new THREE.BloomPass( 0.75 );
+    return [renderPass, effectBloom]
+  }
+  monkeyMesh(){
+    const material= new THREE.PointsMaterial( { color:0x2cfcd6, size:0.015 } )
+    const loader = new THREE.JSONLoader();
+    let monkeyParsed = loader.parse( monkeyJSON );
+    return new THREE.Points( monkeyParsed.geometry, material);
+  }
+  setupCamera(){
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    this.camera.position.z = 3;
+  }
+  setupRenderer(){
+    this.renderer = new THREE.WebGLRenderer({canvas:this.canvas, alpha:true});
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+  }
+  setupComposer(renderPasses = []){
+    this.composer = new THREE.EffectComposer( this.renderer );
+    renderPasses.forEach(
+      renderPass => {
+        this.composer.addPass(renderPass)
+      }
+    )
   }
   animate(){
     requestAnimationFrame( this.animate );
 
-    this._mesh.rotation.x += 0.01;
-    this._mesh.rotation.y += 0.02;
+    this.mesh.rotation.x += 0.001;
+    this.mesh.rotation.y += 0.002;
 
-    this._renderer.render( this._scene, this._camera );
-
+    // this.renderer.clear();
+    // this.composer.render( 0.01 );
+    this.renderer.render( this.scene, this.camera );
   }
   render() {
     return (
-      <canvas ref={ el => { this._canvas = el }} style={{position:'absolute',top:0,left:0}}></canvas>
+      <canvas ref={ el => { this.canvas = el }} style={{position:'absolute',top:0,left:0}}></canvas>
     );
   }
 }
