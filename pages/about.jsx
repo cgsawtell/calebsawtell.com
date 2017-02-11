6632c03e0dfa79abb70af8ca10fa3b285514edde
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-11 17:39:43 +1300

    added faker for filler text

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ContentFrame from '../components/content-frame';
+import faker from 'faker';
 class About extends Component {
   render() {
     return (
       <div>
         <ContentFrame title={"About"}>
-          <p>Poop</p>
+          <p>{faker.lorem.sentences(5)}</p>
+          <p>{faker.lorem.sentences(5)}</p>          
         </ContentFrame>
       </div>
     );
