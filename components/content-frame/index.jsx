b5408bcab6383e61168011d25ae370baf9c19249
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-12 11:25:00 +1300

    fixed issue with referenced to window not working for server side rendering

--- components/content-frame/index.jsx
+++ components/content-frame/index.jsx
@@ -6,7 +6,9 @@ class ContentFrame extends Component {
     super(props);
     this.state = {
       mouseX:0,
-      mouseY:0
+      mouseY:0,
+      windowWidth:1024,
+      windowHeight:768,
     }
     this.handleMouseMove = this.handleMouseMove.bind(this)
   }
@@ -19,20 +21,21 @@ class ContentFrame extends Component {
     )
   }
   componentDidMount() {
+    this.setState({windowWidth:window.outerWidth, windowHeight:window.outerHeight})
     window.addEventListener("mousemove",this.handleMouseMove)
   }
   componentWillUnmount() {
     window.removeEventListener("mousemove",this.handleMouseMove)    
   }
   calculateOffset(mouseX, mouseY, maxOffsetAmount = 2){
-    const halfWindowWidth = window.outerWidth / 2
-    const halfWindowHeight = window.outerHeight / 2
+    const halfWindowWidth = this.state.windowWidth / 2
+    const halfWindowHeight = this.state.windowHeight / 2
     const x =  ((mouseX - halfWindowWidth) / halfWindowWidth) * maxOffsetAmount
     const y =  ((mouseY - halfWindowHeight) / halfWindowHeight) * maxOffsetAmount
     return({x,y})
   }
   render() {
-    const offset=this.calculateOffset(this.state.mouseX,this.state.mouseY)
+    const offset = this.calculateOffset(this.state.mouseX,this.state.mouseY)
     const offsetStyle = {transform:`translate(${offset.x}px, ${offset.y}px)`}
     return (
       <div className={"frame"}>
