9c9e878de83c03e509177bb904e5b12d680c09b6
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-12 14:31:37 +1300

    added content frame to work page

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ContentFrame from '../components/content-frame';
 
 class Work extends Component {
   render() {
     return (
       <div>
-        
+        <ContentFrame title={"Work"}>
+        </ContentFrame>
       </div>
     );
   }
