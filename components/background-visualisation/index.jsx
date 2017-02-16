2735d1b7bc27c0f58e96e70337efcf0e6e3e8d4f
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-16 16:31:46 +1300

    added 3d back in

--- components/background-visualisation/index.jsx
+++ components/background-visualisation/index.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './style.css'
 import backgroundCheckers from "../../assets/background-checkers.svg";
-// import ThreeDee from '../three-dee';
+import ThreeDee from '../three-dee';
 
 class BackgroundVisualisation extends Component {
   render() {
     return (
       <div className={"background-visualisation"} style={{backgroundImage:`url(${backgroundCheckers})`}}>
-        {/* <ThreeDee/>*/}
+        <ThreeDee/>
         <div className={"background-glow"}>
         </div>
       </div>
