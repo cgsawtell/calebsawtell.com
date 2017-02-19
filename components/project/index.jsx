b1c6b17890dde0b72f5e538c92a6fade115206b5
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-20 10:51:47 +1300

    changed props syntax

--- components/project/index.jsx
+++ components/project/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Project = ({images=[], title='', description=''}) => {
+const Project = ({images=[], name='', description=''}) => {
   return (
     <div className={'project-container'}>
-      
+
       <div className={'project-info'}>
-        <h2>{title}</h2>
+        <h3>{name}</h3>
         <p>{description}</p>
       </div>
     </div>
