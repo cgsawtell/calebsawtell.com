36c1e7268ff269c610b5154ea921c0f019410b40
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-20 17:16:47 +1300

    added vimeo video for bloom and gust

--- components/project/index.jsx
+++ components/project/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Vimeo from 'react-vimeo';
+import '../../node_modules/react-vimeo/lib/Vimeo.min.css'
 import './style.css'
-const Project = ({images=[], name='', description=''}) => {
+const Project = ({images=[], name='', description='', vimeoId=''}) => {
   return (
     <div className={'project-container'}>
       <div className={'image-container'}>
-      <img src={images[0]}/>
+        {vimeoId ? <Vimeo videoId = { vimeoId } /> : <img src={images[0]}/>}
       </div>
       <div className={'project-info'}>
         <h3 className={'project-name'}>{name}</h3>
--- pages/work.jsx
+++ pages/work.jsx
@@ -16,8 +16,8 @@ class Work extends Component {
           <ProjectContainer>
             <Project images={[simoneJourneyMap]} name={"Simone"} description={"Simone simulates the New Zealand population and how they intereact with our clients services. My role was to create a frontend for this simulator that is consumable and usable by non data scientists."}/>
             <Project name={"Tesseract"} description={""}/>
-            <Project name={"Blume"} description={""}/>
-            <Project name={"Gust"} description={"An experiment in building a custom hardware controller for a unity game. I used an IR sensor hooked up to an Arduino and wrote a little bridging program in processing to take the data from the arduino and send it to unity. I then made a little demo game to go along with this custom hardware."}/>
+            <Project name={"Blume"} vimeoId={'78698194'} description={""}/>
+            <Project name={"Gust"} vimeoId={'203911242'} description={"An experiment in building a custom hardware controller for a unity game. I used an IR sensor hooked up to an Arduino and wrote a little bridging program in processing to take the data from the arduino and send it to unity. I then made a little demo game to go along with this custom hardware."}/>
           </ProjectContainer>
         </ContentFrame>
       </div>
