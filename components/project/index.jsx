b03f2243b1751c2974ed7e9b498f7aee8e34026a
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-20 14:30:21 +1300

    added image and classes to project component

--- components/project/index.jsx
+++ components/project/index.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const Project = ({images=[], name='', description=''}) => {
   return (
     <div className={'project-container'}>
-
+      <div>
+        <img src={images[0]}/>
+      </div>
       <div className={'project-info'}>
-        <h3>{name}</h3>
-        <p>{description}</p>
+        <h3 className={'project-name'}>{name}</h3>
+        <p className={'project-description'}>{description}</p>
       </div>
     </div>
   );
