786adc61d359b34df55b857848ea0e2cbdba2a67
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-20 15:59:50 +1300

    added styling to project view

--- components/project/index.jsx
+++ components/project/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './style.css'
 const Project = ({images=[], name='', description=''}) => {
   return (
     <div className={'project-container'}>
-      <div>
-        <img src={images[0]}/>
+      <div className={'image-container'}>
+      <img src={images[0]}/>
       </div>
       <div className={'project-info'}>
         <h3 className={'project-name'}>{name}</h3>
