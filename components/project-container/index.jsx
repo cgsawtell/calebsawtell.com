decb60985756c4f181372ab5f7e5babb9cdf0a3a
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-20 14:30:46 +1300

    added project container component for pagination of projects

---
+++ components/project-container/index.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class ProjectContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentProject : 0
+    }
+    this.handleNext = this.handleNext.bind(this)
+    this.handlePrevious = this.handlePrevious.bind(this)
+     
+  }
+  handleNext(){
+    const finalIndex = this.props.children.length - 1
+    const currentProject = this.state.currentProject
+    const nextIndex = currentProject === finalIndex ? 0 : currentProject + 1
+    this.setState({currentProject:nextIndex});
+  }
+  handlePrevious(){
+    const finalIndex = this.props.children.length - 1
+    const currentProject = this.state.currentProject
+    const nextIndex = currentProject === 0 ? finalIndex : currentProject - 1
+    this.setState({currentProject:nextIndex});
+  }
+  render() {
+    return (
+      <div>
+        {this.props.children[this.state.currentProject]}
+        <footer className={'project-container-footer'}>
+          <button onClick={this.handlePrevious}>Previous</button>
+          <p>{this.state.currentProject+1} of {this.props.children.length}</p>
+          <button onClick={this.handleNext}>Next</button>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default ProjectContainer;
