f02de67e164fe844aa19871b1ad00976bb84da93
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-21 15:17:41 +1300

    added gifs for tesseract

--- pages/work.jsx
+++ pages/work.jsx
@@ -7,6 +7,8 @@ import Helmet from 'react-helmet'
 import simoneJourneyMap from '../assets/screenshots/simone/simone-journey-map.png'
 import simoneSimulate from '../assets/screenshots/simone/simone-simulate.png'
 import simoneDashboard from '../assets/screenshots/simone/simone-dashboard.png'
+import tesseractSpinning from '../assets/screenshots/tesseract/Spinning.gif'
+import tesseractLookingAtStakeholder from '../assets/screenshots/tesseract/LookingAtStakeholder.gif'
 
 class Work extends Component {
   render() {
@@ -18,7 +20,7 @@ class Work extends Component {
         <ContentFrame title={"Work"}>
           <ProjectContainer>
             <Project images={[simoneJourneyMap, simoneDashboard,simoneSimulate]} name={"Simone"} description={"Simone simulates the New Zealand population and how they intereact with our clients services. My role was to create a frontend for this simulator that is consumable and usable by non data scientists."}/>
-            <Project name={"Tesseract"} description={`Tesseract is a prototype stakeholder management tool. The design for tesseract was inspired by fictional UI's seen in movies like "The Martian" and "James Bond". We were trying to make the user feel like a badass and the tool look high tech`}/>
+            <Project name={"Tesseract"} images={[tesseractSpinning,tesseractLookingAtStakeholder]} description={`Tesseract is a prototype stakeholder management tool. The design for tesseract was inspired by fictional UI's seen in movies like "The Martian" and "James Bond". We were trying to make the user feel like a badass and the tool look high tech`}/>
             <Project name={"Blume"} vimeoId={'78698194'} description={`Blume is an explorative, narrative-driven game focusing on retrograde amnesia and the impact it has on relationships. Entering the world of Blume, the player navigates through the darkness with their ethereal light companion, exploring the landscape in search of lost memories.`}/>
             <Project name={"Gust"} vimeoId={'203911242'} description={"An experiment in building a custom hardware controller for a unity game. I used an IR sensor hooked up to an Arduino and wrote a little bridging program in processing to take the data from the arduino and send it to unity. I then made a little demo game to go along with this custom hardware."}/>
           </ProjectContainer>
