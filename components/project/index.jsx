ee7e914c5028fd996ef5a27c2e422fbfd111eeda
Caleb Sawtell <caleb.sawtell@gmail.com>
2017-02-21 15:04:57 +1300

    added image slider and discriptions for tessaract and blume

--- components/project/index.jsx
+++ components/project/index.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Vimeo from 'react-vimeo';
 import '../../node_modules/react-vimeo/lib/Vimeo.min.css'
+import Slider from 'react-slick'
+import '../../node_modules/slick-carousel/slick/slick.css'
+import '../../node_modules/slick-carousel/slick/slick-theme.css'
 import './style.css'
+
 const Project = ({images=[], name='', description='', vimeoId=''}) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  let ImageSlider = ''
+
+  if(images.length){
+   ImageSlider = <Slider {...settings}>
+      {
+        images.map( image => ( <img src={image}/>  ) )
+      }
+    </Slider>
+  }
+
   return (
-    <div className={'project-container'}>
+    <div className={'project'}>
       <div className={'image-container'}>
-        {vimeoId ? <Vimeo videoId = { vimeoId } /> : <img src={images[0]}/>}
+        {vimeoId ? <Vimeo videoId = { vimeoId } /> : ImageSlider}
       </div>
       <div className={'project-info'}>
         <h3 className={'project-name'}>{name}</h3>
--- pages/work.jsx
+++ pages/work.jsx
@@ -5,6 +5,9 @@ import ProjectContainer from '../components/project-container';
 import { config } from 'config'
 import Helmet from 'react-helmet'
 import simoneJourneyMap from '../assets/screenshots/simone/simone-journey-map.png'
+import simoneSimulate from '../assets/screenshots/simone/simone-simulate.png'
+import simoneDashboard from '../assets/screenshots/simone/simone-dashboard.png'
+
 class Work extends Component {
   render() {
     return (
@@ -14,9 +17,9 @@ class Work extends Component {
         />
         <ContentFrame title={"Work"}>
           <ProjectContainer>
-            <Project images={[simoneJourneyMap]} name={"Simone"} description={"Simone simulates the New Zealand population and how they intereact with our clients services. My role was to create a frontend for this simulator that is consumable and usable by non data scientists."}/>
-            <Project name={"Tesseract"} description={""}/>
-            <Project name={"Blume"} vimeoId={'78698194'} description={""}/>
+            <Project images={[simoneJourneyMap, simoneDashboard,simoneSimulate]} name={"Simone"} description={"Simone simulates the New Zealand population and how they intereact with our clients services. My role was to create a frontend for this simulator that is consumable and usable by non data scientists."}/>
+            <Project name={"Tesseract"} description={`Tesseract is a prototype stakeholder management tool. The design for tesseract was inspired by fictional UI's seen in movies like "The Martian" and "James Bond". We were trying to make the user feel like a badass and the tool look high tech`}/>
+            <Project name={"Blume"} vimeoId={'78698194'} description={`Blume is an explorative, narrative-driven game focusing on retrograde amnesia and the impact it has on relationships. Entering the world of Blume, the player navigates through the darkness with their ethereal light companion, exploring the landscape in search of lost memories.`}/>
             <Project name={"Gust"} vimeoId={'203911242'} description={"An experiment in building a custom hardware controller for a unity game. I used an IR sensor hooked up to an Arduino and wrote a little bridging program in processing to take the data from the arduino and send it to unity. I then made a little demo game to go along with this custom hardware."}/>
           </ProjectContainer>
         </ContentFrame>
